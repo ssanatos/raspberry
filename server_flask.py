3800fc2b0ba928242d16516238352f8464f4ac89
내 이름 <you@example.com>
2021-12-10 09:36:46 +0900

    commit

--- server_flask.py
+++ server_flask.py
@@ -4,11 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def helloworld():
-    str = "Hello World!"
+    str = "Hello World! SanghunOh"
     return str
 
-# host가 ip. 다른데서도 막 접속하게 0.0.0.0 
-# 저 플라스크를 서버인양 동작하기
-app.run(host = '0.0.0.0', port = '8000')
-# 실행하면 터미널에 나오는 IP 클릭해서 들어가기.
-# ----
+app.run(host='0.0.0.0', port='8000')
--- thread_001.py
+++ thread_001.py
@@ -2,22 +2,27 @@ import threading
 
 def first_fun(age):
     while True:
-        print('I am first child.', age)
+        print("I am first child", age)
     return
 
 def second_fun(age):
     while True:
-        print('I am second child.', age)
+        print("I am second child",age)
     return
 
-if __name__ == '__main__':
+if __name__ == '__main__' :
     first = threading.Thread(target=first_fun, args=(5,))
     second = threading.Thread(target=second_fun, args=(3,))
+    # 스레드 활성화
     first.start()
     second.start()
+    # 파라미터 설정
+    first.daemon = True
+    second.daemon = True
+    # 스레드 끌어옴
     first.join()
     second.join()
 
-    while True :
-        print('I am parent.')
+    while True:
+        print('I am parent')
     pass
